a5ef4aa5aa300aac772beb770564f0b365027156
Louis S. Berman <louis@squideyes.com>
2014-04-20 16:43:25 -0400

    Removed DevExpress dependency from SquidEyes.GUI

---
+++ Source/SquidEyes.Generic/Helpers/CsvFileReader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Collections;
+
+namespace SquidEyes.Generic
+{
+    public class CsvFileReader: IEnumerable<string[]>
+    {
+        private StreamReader reader;
+
+        public CsvFileReader(string fileName)
+        {
+            reader = new StreamReader(fileName);
+        }
+
+        public IEnumerator<string[]> GetEnumerator()
+        {
+            string line;
+            
+            while ((line = reader.ReadLine()) != null)
+                yield return line.Split(',');
+
+            yield return null;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
